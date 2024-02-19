e220e0db9f393baba8d6c7a1431ec700cc754bb0
Arthur Carneiro Lobo <arthurlbo16@gmail.com>
2024-02-18 23:12:24 -0300

    feat: adding shadow gradient

--- src/components/moving-techs.tsx
+++ src/components/moving-techs.tsx
@@ -41,7 +41,7 @@ export const MovingTechs = () => {
     const extendedTechs = techs.concat(techs);
 
     return (
-        <section className="flex h-20 w-full max-w-[100vw] items-center justify-center overflow-x-hidden border-y border-y-hover">
+        <section className="relative flex h-20 w-full max-w-[100vw] items-center justify-center overflow-x-hidden border-y border-y-hover">
             <ul className="flex h-full w-full animate-infinity-card items-center justify-between gap-6 hover:[animation-play-state:paused] lg:animate-infinity-card-lg">
                 {extendedTechs.map((tech, index) => (
                     <li key={index} className="flex flex-none items-center gap-6">
@@ -50,6 +50,8 @@ export const MovingTechs = () => {
                     </li>
                 ))}
             </ul>
+            <div className="absolute inset-y-0 left-0 w-14 bg-gradient-to-r from-black to-transparent" />
+            <div className="absolute inset-y-0 right-0 w-14 bg-gradient-to-l from-black to-transparent" />
         </section>
     );
 };
