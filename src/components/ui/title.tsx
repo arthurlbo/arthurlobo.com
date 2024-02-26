9fc3edf948f5476bd4b8435ccab5ee472f2b8f84
Arthur Carneiro Lobo <arthurlbo16@gmail.com>
2024-02-26 12:02:00 -0300

    feat: adding title align variant

--- src/components/ui/title.tsx
+++ src/components/ui/title.tsx
@@ -1,14 +1,29 @@
+import { cn } from "@/utils/cn";
+import { VariantProps, cva } from "class-variance-authority";
+
 import { IconType } from "@/lib/iconType";
 
-interface TitleProps {
+interface TitleProps extends React.ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof titleVariants> {
     title: string;
     label: string;
     icon: IconType;
 }
 
-export const Title = ({ title, label, icon: Icon }: TitleProps) => {
+const titleVariants = cva("flex w-full flex-col gap-3", {
+    variants: {
+        align: {
+            start: "items-start md:items-center lg:items-start",
+            center: "items-center",
+        },
+    },
+    defaultVariants: {
+        align: "start",
+    },
+});
+
+export const Title = ({ title, label, icon: Icon, align }: TitleProps) => {
     return (
-        <div className="flex w-full flex-col items-start md:items-center lg:items-start gap-3">
+        <div className={cn(titleVariants({ align }))}>
             <div className="flex items-center gap-1 text-secondary">
                 <Icon className="h-4 w-4" />
                 <h2 className="text-sm font-bold leading-tight">{label}</h2>
