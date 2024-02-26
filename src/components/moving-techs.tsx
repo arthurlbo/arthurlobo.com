982f5428d4a6c4701518068801a8eaf17731641e
Arthur Carneiro Lobo <arthurlbo16@gmail.com>
2024-02-26 11:19:03 -0300

    refactor: changing hover to the parent container

--- src/components/moving-techs.tsx
+++ src/components/moving-techs.tsx
@@ -41,8 +41,8 @@ export const MovingTechs = () => {
     const extendedTechs = techs.concat(techs);
 
     return (
-        <section className="relative flex h-20 w-full max-w-[100vw] items-center justify-center overflow-x-hidden border-y border-y-hover">
-            <ul className="flex h-full w-full animate-infinity-card items-center justify-between gap-6 hover:[animation-play-state:paused] lg:animate-infinity-card-lg">
+        <section className="relative flex h-20 w-full max-w-[100vw] items-center justify-center overflow-x-hidden border-y border-y-hover group">
+            <ul className="flex h-full w-full animate-infinity-card items-center justify-between gap-6 group-hover:[animation-play-state:paused] lg:animate-infinity-card-lg">
                 {extendedTechs.map((tech, index) => (
                     <li key={index} className="flex flex-none items-center gap-6">
                         <IconBolt className="h-4 w-4 flex-none fill-secondary text-secondary" />
