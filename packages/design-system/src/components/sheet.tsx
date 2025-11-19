5dc0339eab5b46a64b0bba1f0fb64c6d1f42c2b3
arthurlbo <arthurlbo16@gmail.com>
2025-11-18 22:29:35 -0300

    refactor(web): fixing drawer style

--- packages/design-system/src/components/sheet.tsx
+++ packages/design-system/src/components/sheet.tsx
@@ -22,15 +22,22 @@ function SheetPortal({ ...props }: React.ComponentProps<typeof SheetPrimitive.Po
     return <SheetPrimitive.Portal data-slot="sheet-portal" {...props} />;
 }
 
+// Radix for some reason its overwriting the styles, the bg, inset and z-index are not being applied, need to set them inlined.
 function SheetOverlay({ className, ...props }: React.ComponentProps<typeof SheetPrimitive.Overlay>) {
     return (
         <SheetPrimitive.Overlay
             data-slot="sheet-overlay"
             className={cn(
-                "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-black/50",
+                "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 backdrop-blur-sm  fixed inset-0 z-50 bg-black/50",
                 className,
             )}
             {...props}
+            style={{
+                position: "fixed",
+                inset: 0,
+                zIndex: 40,
+                backgroundColor: "rgba(0, 0, 0, 0.5)",
+            }}
         />
     );
 }
@@ -53,7 +60,7 @@ function SheetContent({
                 className={cn(
                     "bg-background data-[state=open]:animate-in data-[state=closed]:animate-out border-border fixed z-50 flex max-h-screen flex-col gap-4 overflow-y-scroll border-b transition ease-in-out data-[state=closed]:duration-300 data-[state=open]:duration-500",
                     side === "right" &&
-                        "data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right inset-y-0 right-0 h-full w-3/4 border-l sm:max-w-sm",
+                        "data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right inset-y-0 right-0 h-full w-full border-l",
                     side === "left" &&
                         "data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left inset-y-0 left-0 h-full w-3/4 border-r sm:max-w-sm",
                     side === "top" &&
