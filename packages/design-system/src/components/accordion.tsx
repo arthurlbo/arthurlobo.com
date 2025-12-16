1932a18abf039cf7fbc5f59728e207c276ce3e35
arthurlbo <arthurlbo16@gmail.com>
2025-12-15 22:00:18 -0300

    style: improving format config

--- packages/design-system/src/components/accordion.tsx
+++ packages/design-system/src/components/accordion.tsx
@@ -20,21 +20,27 @@ function AccordionItem({ className, ...props }: React.ComponentProps<typeof Acco
     );
 }
 
-function AccordionTrigger({ className, children, ...props }: React.ComponentProps<typeof AccordionPrimitive.Trigger>) {
+function AccordionTrigger({
+    className,
+    children,
+    showIcon = true,
+    ...props
+}: React.ComponentProps<typeof AccordionPrimitive.Trigger> & { showIcon?: boolean }) {
     return (
-        <AccordionPrimitive.Header className="flex">
-            <AccordionPrimitive.Trigger
-                data-slot="accordion-trigger"
-                className={cn(
-                    "focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-start justify-between gap-4 rounded-md py-4 text-left text-sm font-medium transition-all outline-none hover:underline focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&[data-state=open]>svg]:rotate-180",
-                    className,
-                )}
-                {...props}
-            >
-                {children}
-                <ChevronDownIcon className="text-muted-foreground pointer-events-none size-4 shrink-0 translate-y-0.5 transition-transform duration-200" />
-            </AccordionPrimitive.Trigger>
-        </AccordionPrimitive.Header>
+        <AccordionPrimitive.Trigger
+            data-slot="accordion-trigger"
+            className={cn(
+                "focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-start justify-between gap-4 rounded-md py-4 text-left text-sm font-medium transition-all outline-none focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&[data-state=open]>svg]:rotate-180",
+                className,
+            )}
+            {...props}
+        >
+            {children}
+
+            {showIcon && (
+                <ChevronDownIcon className="text-primary-100 pointer-events-none size-5 shrink-0 transition-all duration-300" />
+            )}
+        </AccordionPrimitive.Trigger>
     );
 }
 
