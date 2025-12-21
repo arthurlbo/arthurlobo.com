5cc612506a115433ca4fb28c04bfb7c9778cfdb8
arthurlbo <arthurlbo16@gmail.com>
2025-12-20 22:33:38 -0300

    refactor(web): adding max validations

--- apps/web/src/features/contact/form/schema.ts
+++ apps/web/src/features/contact/form/schema.ts
@@ -1,9 +1,15 @@
 import z from "zod";
 
 export const contactFormSchema = z.object({
-    name: z.string().min(2, "Name must be at least 2 characters long"),
+    name: z
+        .string()
+        .min(2, "Name must be at least 2 characters long")
+        .max(100, "Name must be at most 100 characters long"),
     email: z.email({ error: "Enter a valid email address" }),
-    message: z.string().min(10, "Message must be at least 10 characters long"),
+    message: z
+        .string()
+        .min(10, "Message must be at least 10 characters long")
+        .max(500, "Message must be at most 500 characters long"),
 });
 
 export type TContactFormData = z.infer<typeof contactFormSchema>;
