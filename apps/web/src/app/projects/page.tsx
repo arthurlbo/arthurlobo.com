eb546936b3709e64cd993a3e878a462ff59cee1a
arthurlbo <arthurlbo16@gmail.com>
2025-12-26 22:56:30 -0300

    Revert "fix(web): removing SSG to run first build"
    This reverts commit 50b98ea45a9e3a81296a3fd24db5214132310f93.

--- apps/web/src/app/projects/page.tsx
+++ apps/web/src/app/projects/page.tsx
@@ -4,8 +4,7 @@ import { Heading } from "@/shared/components/ui";
 
 import { ProjectsShowcase, getProjects } from "@/features/projects";
 
-// TODO - Change to force-static after first website build
-export const dynamic = "force-dynamic";
+export const dynamic = "force-static";
 export const revalidate = 86400; // 1 day (60 * 60 * 24)
 
 export const metadata: Metadata = {
