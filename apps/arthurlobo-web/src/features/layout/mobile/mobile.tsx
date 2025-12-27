4f0bdedf8d103e1aa17fefc00efb2a214ed3a297
arthurlbo <arthurlbo16@gmail.com>
2025-12-27 15:38:40 -0300

    feat(web): make profile image link to home page

--- apps/arthurlobo-web/src/features/layout/mobile/mobile.tsx
+++ apps/arthurlobo-web/src/features/layout/mobile/mobile.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 import Me from "@/assets/navigation/me.webp";
 
@@ -13,14 +14,16 @@ export const Mobile = () => (
         className="border-surface-500 bg-background/50 fixed top-0 left-0 z-50 flex h-[72px] w-full items-center justify-between border-b p-4 backdrop-blur-sm xl:hidden"
     >
         <div className="border-surface-500 relative h-[50px] w-[50px] shrink-0 overflow-hidden rounded-full border">
-            <Image
-                src={Me}
-                alt="Profile"
-                priority
-                quality={100}
-                placeholder="blur"
-                className="h-full w-full object-cover object-top"
-            />
+            <Link href="/">
+                <Image
+                    src={Me}
+                    alt="Profile"
+                    priority
+                    quality={100}
+                    placeholder="blur"
+                    className="h-full w-full object-cover object-top"
+                />
+            </Link>
         </div>
 
         <Drawer />
