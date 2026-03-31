930f4af9ce5d370c546287a881098678ea88edaa
arthurlbo <arthurlbo16@gmail.com>
2026-03-31 09:48:08 -0300

    refactor(web): improving graph ui

--- apps/arthurlobo-web/src/features/dashboard/contribution/graph.tsx
+++ apps/arthurlobo-web/src/features/dashboard/contribution/graph.tsx
@@ -17,7 +17,7 @@ const ACTIVITY_LEVEL_COLORS: Record<number, string> = {
 export const Graph = ({ months, weeks }: IProps) => {
     return (
         <div className="border-surface-500 bg-surface-700/30 flex w-full flex-col gap-2 rounded-2xl border p-6">
-            <div className="text-primary-300 flex w-full items-start justify-between gap-4 overflow-x-auto text-sm md:gap-0">
+            <div className="text-primary-300 flex w-full items-start justify-between gap-4 overflow-x-auto text-xs md:gap-0">
                 {months.map((month, index) => (
                     <span key={index} style={{ flex: month.totalWeeks }} className="text-center">
                         {month.name}
@@ -25,15 +25,15 @@ export const Graph = ({ months, weeks }: IProps) => {
                 ))}
             </div>
 
-            <div className="flex w-full justify-between gap-2 overflow-x-auto lg:gap-0">
+            <div className="flex w-full justify-between gap-2 overflow-x-auto lg:gap-0 lg:overflow-x-visible">
                 {weeks.map((week, weekIdx) => (
-                    <div key={weekIdx} className="flex w-4 flex-col gap-1">
+                    <div key={weekIdx} className="flex flex-col gap-1">
                         {week.map((day, dayIdx) => (
                             <Tooltip key={dayIdx} delayDuration={100} disableHoverableContent>
                                 <TooltipTrigger asChild>
                                     <div
                                         className={cn(
-                                            "ring-offset-surface-700 h-4 w-4 cursor-pointer rounded-[5px] ring-offset-2 transition-all duration-300 ease-in-out hover:scale-105 hover:ring-2 hover:ring-[#39d353]",
+                                            "ring-offset-surface-700 h-[14px] w-[14px] cursor-pointer rounded-[4px] ring-offset-2 transition-all duration-300 ease-in-out hover:scale-105 hover:ring-2 hover:ring-[#39d353]",
                                             ACTIVITY_LEVEL_COLORS[day.level],
                                         )}
                                     />
@@ -55,12 +55,12 @@ export const Graph = ({ months, weeks }: IProps) => {
                 ))}
             </div>
 
-            <div className="text-primary-200 mt-2 flex w-full items-center justify-end gap-2 text-xs">
+            <div className="text-primary-300 mt-1 flex w-full items-center justify-end gap-2 text-xs">
                 <span>Less</span>
 
                 <div className="flex gap-1">
                     {Object.values(ACTIVITY_LEVEL_COLORS).map((color, index) => (
-                        <div key={index} className={cn("h-3 w-3 rounded-[4px]", color)} />
+                        <div key={index} className={cn("h-[14px] w-[14px] rounded-[4px]", color)} />
                     ))}
                 </div>
 
